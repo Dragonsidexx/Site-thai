462f0cde92207a4624616368049818bf18425628
Munizz_.vr <144976870+Dragonsidexx@users.noreply.github.com>
2024-08-05 08:28:12 -0300

    Update Thai2.js

--- Thai2.js
+++ Thai2.js
@@ -1,5 +1,25 @@
 window.onload = function() {
     let questionAnswered = false;
+    const redirectURL = 'DeclraThai.html';  // Define o URL de redirecionamento aqui
+
+    function showModal(message) {
+        const modal = document.getElementById('customAlert');
+        const modalText = document.getElementById('modalText');
+        modalText.textContent = message;
+        modal.style.display = 'block';
+    }
+
+    document.querySelector('.close-button').onclick = function() {
+        const modal = document.getElementById('customAlert');
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        const modal = document.getElementById('customAlert');
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    }
 
     function checkAnswer() {
         const answer = document.getElementById('answer').value;
@@ -7,9 +27,11 @@ window.onload = function() {
             questionAnswered = true;
             document.getElementById('response').textContent = answer;
             document.getElementById('responseContainer').style.display = 'block';
-            window.location.href = 'DeclraThai.html';
+            setTimeout(function() {
+                window.location.href = redirectURL;
+            }, 2000);  // Ajuste o tempo conforme necessário
         } else {
-            alert('Por favor, responda a pergunta antes de sair.');
+            showModal('Por favor, responda a pergunta antes de sair.');
         }
     }
 
@@ -21,7 +43,25 @@ window.onload = function() {
         }
     }
 
-    document.getElementById('Enviar').onclick = function() {
+    document.getElementById('Enviar').onclick = function(event) {
+        event.preventDefault();
         checkAnswer();
+    }
+
+    const toggleVideoButton = document.getElementById('toggleVideo');
+    const videoContainer = document.getElementById('videoContainer');
+
+    // Inicialmente esconder o vídeo
+    videoContainer.style.display = 'none';
+    toggleVideoButton.textContent = 'Mostrar Vídeo';
+
+    toggleVideoButton.onclick = function() {
+        if (videoContainer.style.display === 'none') {
+            videoContainer.style.display = 'block';
+            toggleVideoButton.textContent = 'Esconder Vídeo';
+        } else {
+            videoContainer.style.display = 'none';
+            toggleVideoButton.textContent = 'Mostrar Vídeo';
+        }
     }
 }
