ab103f2585a87a99614983474f6dd25d2f0eca52
Munizz_.vr <144976870+Dragonsidexx@users.noreply.github.com>
2024-08-12 10:07:57 -0300

    Update Thai.js

--- Thai.js
+++ Thai.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     let questionAnswered = false;
-    let redirectURL = 'Thai2.html';  // Define o URL de redirecionamento aqui
+    let redirectURL = 'Ocomeço.html';  // Define o URL de redirecionamento aqui
 
     function showModal(message) {
         const modal = document.getElementById('customAlert');
