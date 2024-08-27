37937f2d29c46bafa7ee99f11c99cada66a41118
Munizz_.vr <144976870+Dragonsidexx@users.noreply.github.com>
2024-08-27 14:26:24 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,21 @@
+// Seleção dos elementos do DOM
 const form = document.querySelector('form');
 const usernameInput = document.querySelector('#username');
 const passwordInput = document.querySelector('#password');
 const togglePassword = document.querySelector('#togglePassword');
 
+// Função para validar credenciais
 function validateCredentials(username, password) {
-    if (username.trim() === '' || password.trim() === '') {
-        return false;
-    }
-    return true;
+    // Verifica se o nome de usuário e a senha não estão vazios
+    return username.trim() !== '' && password.trim() !== '';
 }
 
+// Função para redirecionar para a página de dashboard
 function redirectDashboard() {
     window.location.href = 'filmes.html';
 }
 
+// Manipulador do evento de submit do formulário
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const username = usernameInput.value;
@@ -28,8 +30,12 @@ form.addEventListener('submit', (e) => {
     }
 });
 
+// Manipulador do evento de clique no ícone de olho
 togglePassword.addEventListener('click', () => {
+    // Alterna o tipo do campo de senha entre 'password' e 'text'
     const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordInput.setAttribute('type', type);
-    togglePassword.textContent = type === 'password' ? '👁️' : '👁️‍🗨️'; // Change icon based on visibility
+    
+    // Alterna o ícone de olho com base na visibilidade da senha
+    togglePassword.textContent = type === 'password' ? '👁️' : '🙈'; // Atualiza o ícone
 });
